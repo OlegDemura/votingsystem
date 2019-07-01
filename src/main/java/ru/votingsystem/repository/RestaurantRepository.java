43824d39b043dacce3f2667f291d59ec63a6230b
Олег Демура <demonshine1983@gmail.com>
2019-07-01 22:21:46 +0900

    Create interface for Repository.

---
+++ src/main/java/ru/votingsystem/repository/RestaurantRepository.java
@@ -0,0 +1,20 @@
+package ru.votingsystem.repository;
+
+import ru.votingsystem.model.Restaurant;
+
+import java.util.List;
+
+public interface RestaurantRepository{
+
+    // null if not found, when updated
+    Restaurant save(Restaurant restaurant);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Restaurant get(int id);
+
+    List<Restaurant> getAll();
+
+}
---
+++ src/main/java/ru/votingsystem/repository/VoteRepository.java
@@ -0,0 +1,19 @@
+package ru.votingsystem.repository;
+
+import ru.votingsystem.model.Vote;
+
+import java.util.List;
+
+
+public interface VoteRepository {
+    // null if not found, when updated
+    Vote save(Vote vote);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Vote get(int id);
+
+    List<Vote> getAll();
+}
