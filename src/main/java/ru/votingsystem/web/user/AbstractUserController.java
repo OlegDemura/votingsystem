e86d09ed0a3ab248b22a484f337d111e942f48ba
Олег Демура <demonshine1983@gmail.com>
2019-07-02 22:12:34 +0900

    Create AbstractUserController.

---
+++ src/main/java/ru/votingsystem/web/user/AbstractUserController.java
@@ -0,0 +1,53 @@
+package ru.votingsystem.web.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.votingsystem.model.User;
+import ru.votingsystem.service.UserService;
+
+import java.util.List;
+
+import static ru.votingsystem.util.ValidationUtil.assureIdConsistent;
+import static ru.votingsystem.util.ValidationUtil.checkNew;
+
+@Controller
+public abstract class AbstractUserController {
+    private final Logger log = LoggerFactory.getLogger(AbstractUserController.class);
+
+    @Autowired
+    private UserService service;
+
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        return service.create(user);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(User user, int id) {
+        log.info("update {} with id={}", user, id);
+        assureIdConsistent(user, id);
+        service.update(user);
+    }
+
+    public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        return service.getByEmail(email);
+    }
+}
