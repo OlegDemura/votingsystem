a3e50ad6aa4b03f5935fd90a6482a1437ebc0769
Олег Демура <demonshine1983@gmail.com>
2019-07-02 22:42:56 +0900

    Create JpaMealRepository

---
+++ src/main/java/ru/votingsystem/repository/jpa/JpaMealRepository.java
@@ -0,0 +1,60 @@
+package ru.votingsystem.repository.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.votingsystem.model.Meal;
+import ru.votingsystem.model.Restaurant;
+import ru.votingsystem.repository.MealRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class JpaMealRepository implements MealRepository {
+
+    @Autowired
+    private EntityManager em;
+
+
+    @Override
+    @Transactional
+    public Meal save(Meal meal, int restaurantId) {
+        if (!meal.isNew() && get(meal.getId(), restaurantId) == null) {
+            return null;
+        }
+        meal.setRestaurant(em.getReference(Restaurant.class, restaurantId));
+
+        if (meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        } else {
+            return em.merge(meal);
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(int id, int restaurantId) {
+        return em.createNamedQuery(Meal.DELETE, Meal.class)
+                .setParameter("id", id)
+                .setParameter("restaurantId", restaurantId)
+                .executeUpdate()!=0;
+    }
+
+    @Override
+    public Meal get(int id, int restaurantId) {
+        List<Meal> list = em.createNamedQuery(Meal.GET, Meal.class)
+                .setParameter("id", id)
+                .setParameter("restaurantId", restaurantId)
+                .getResultList();
+        return DataAccessUtils.singleResult(list);
+    }
+
+    @Override
+    public List<Meal> getAll(int restaurantId) {
+        return em.createNamedQuery(Meal.ALL_SORTED, Meal.class).getResultList();
+    }
+}
