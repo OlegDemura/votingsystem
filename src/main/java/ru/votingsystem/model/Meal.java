7d7f21d992a7d9f2d81c2a496c07bd0e334dcdbf
Олег Демура <demonshine1983@gmail.com>
2019-07-02 22:42:05 +0900

    Fixed class Meal.

--- src/main/java/ru/votingsystem/model/Meal.java
+++ src/main/java/ru/votingsystem/model/Meal.java
@@ -6,14 +6,16 @@ import java.time.LocalDateTime;
 
 @NamedQueries({
         @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id"),
-        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.restaurant.id = :restaurantId ORDER BY m.name DESC")
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.restaurant.id = :restaurantId ORDER BY m.name DESC"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m WHERE m.id = :id AND m.restaurant.id = :restaurantId")
 })
 
 @Entity
 @Table(name = "meals")
 public class Meal extends AbstractNamedEntity {
     public static final String DELETE = "Meal.Delete";
-    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String ALL_SORTED = "Meal.GetAllSorted";
+    public static final String GET = "Meal.Get";
 
     @Column(name = "price")
     @NotNull
