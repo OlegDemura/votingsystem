cfd6a89917c041c99f95e08439d2120311fdbf7c
Олег Демура <demonshine1983@gmail.com>
2019-07-02 22:43:21 +0900

    Create JpaRestaurantRepository

---
+++ src/main/java/ru/votingsystem/repository/jpa/JpaRestaurantRepository.java
@@ -0,0 +1,48 @@
+package ru.votingsystem.repository.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.votingsystem.model.Restaurant;
+import ru.votingsystem.repository.RestaurantRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class JpaRestaurantRepository implements RestaurantRepository {
+
+    @Autowired
+    private EntityManager em;
+
+    @Override
+    @Transactional
+    public Restaurant save(Restaurant restaurant) {
+        if (restaurant.isNew()) {
+            em.persist(restaurant);
+            return restaurant;
+        } else {
+            return em.merge(restaurant);
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(int id) {
+        return em.createNamedQuery(Restaurant.DELETE, Restaurant.class)
+                .setParameter("id", id)
+                .executeUpdate() != 0;
+    }
+
+    @Override
+    public Restaurant get(int id) {
+        return em.find(Restaurant.class, id);
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        return em.createNamedQuery(Restaurant.ALL_SORTED, Restaurant.class)
+                .getResultList();
+    }
+}
