49c83bb95b8798db8f785ecf549fde1a0e915958
Олег Демура <demonshine1983@gmail.com>
2019-07-03 21:42:56 +0900

    Fixed VoteRepository

--- src/main/java/ru/votingsystem/repository/VoteRepository.java
+++ src/main/java/ru/votingsystem/repository/VoteRepository.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 public interface VoteRepository {
     // null if not found, when updated
-    Vote save(Vote vote);
+    Vote save(Vote vote, int userId, int restaurantId);
 
     // false if not found
-    boolean delete(int id);
+    boolean delete(int id, int userId, int restaurantId);
 
     // null if not found
-    Vote get(int id);
+    Vote get(int id, int restaurantId);
 
-    List<Vote> getAll();
+    List<Vote> getAll(int restaurantId);
 }
