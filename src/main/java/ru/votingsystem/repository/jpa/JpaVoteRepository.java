fc514c73dd70e579216bb569989f1c9bf61391b3
Олег Демура <demonshine1983@gmail.com>
2019-07-03 21:43:18 +0900

    Create JpaVoteRepository

---
+++ src/main/java/ru/votingsystem/repository/jpa/JpaVoteRepository.java
@@ -0,0 +1,66 @@
+package ru.votingsystem.repository.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.votingsystem.model.Restaurant;
+import ru.votingsystem.model.Vote;
+import ru.votingsystem.repository.VoteRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class JpaVoteRepository implements VoteRepository {
+
+    @Autowired
+    private EntityManager em;
+
+    @Override
+    @Transactional
+    public Vote save(Vote vote, int userId, int restaurantId) {
+        if (!vote.isNew() && get(vote.getId(), restaurantId) == null) {
+            return null;
+        }
+
+        vote.setRestaurant(em.getReference(Restaurant.class, restaurantId));
+        vote.setUserId(userId);
+        vote.setRestaurantId(restaurantId);
+
+        if (vote.isNew()) {
+            em.persist(vote);
+            return vote;
+        } else {
+            return em.merge(vote);
+        }
+
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(int id, int userId, int restaurantId) {
+        return em.createNamedQuery(Vote.DELETE, Vote.class)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .setParameter("restaurantId", restaurantId)
+                .executeUpdate() != 0;
+    }
+
+    @Override
+    public Vote get(int id, int restaurantId) {
+        List<Vote> list = em.createNamedQuery(Vote.GET, Vote.class)
+                .setParameter("id", id)
+                .setParameter("restaurantId", restaurantId)
+                .getResultList();
+        return DataAccessUtils.singleResult(list);
+    }
+
+    @Override
+    public List<Vote> getAll(int restaurantId) {
+        return em.createNamedQuery(Vote.ALL_SORTED, Vote.class)
+                .setParameter("restaurantId", restaurantId)
+                .getResultList();
+    }
+}
