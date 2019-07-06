41c8499838d4653b76edfca935db15dae9054743
Олег Демура <demonshine1983@gmail.com>
2019-07-06 18:13:50 +0900

    Fixed all directory repository/jpa.

--- src/main/java/ru/votingsystem/repository/jpa/JpaRestaurantRepository.java
+++ src/main/java/ru/votingsystem/repository/jpa/JpaRestaurantRepository.java
@@ -1,6 +1,5 @@
 package ru.votingsystem.repository.jpa;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.votingsystem.model.Restaurant;
--- src/main/java/ru/votingsystem/repository/jpa/JpaUserRepository.java
+++ src/main/java/ru/votingsystem/repository/jpa/JpaUserRepository.java
@@ -1,6 +1,5 @@
 package ru.votingsystem.repository.jpa;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
