d2b6c9da33e0168cf587d70cc7c49ffa598d29a1
Олег Демура <demonshine1983@gmail.com>
2019-07-07 21:34:37 +0900

    Create CrudMealRepository

---
+++ src/main/java/ru/votingsystem/repository/datajpa/CrudMealRepository.java
@@ -0,0 +1,29 @@
+package ru.votingsystem.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.votingsystem.model.Meal;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    int delete(@Param("id") int id, @Param("restaurantId") int restaurantId);
+
+    @Transactional
+    @Modifying
+    Meal save(Meal meal);
+
+    @Query("SELECT m FROM Meal m WHERE m.restaurant.id = :restaurantId ORDER BY m.description")
+    List<Meal> getAll(@Param("restaurantId") int restaurantId);
+
+    @Query("SELECT m FROM Meal m WHERE m.id = :id AND m.restaurant.id = :restaurantId")
+    Meal getOneMeal(@Param("id") int id, @Param("restaurantId") int restaurantId);
+}
+
