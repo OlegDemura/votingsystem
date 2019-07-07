c9968d2fd1c4394b337693dbf6a0e9ec37568e5e
Олег Демура <demonshine1983@gmail.com>
2019-07-07 20:12:00 +0900

    Create AdminRestControllerTest - not done.

---
+++ src/test/java/ru/votingsystem/web/user/AdminRestControllerTest.java
@@ -0,0 +1,32 @@
+package ru.votingsystem.web.user;
+
+import org.junit.Test;
+
+
+public class AdminRestControllerTest {
+
+    @Test
+    public void getAll() {
+
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getByMail() {
+    }
+}
