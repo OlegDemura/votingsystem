f2c6d9e50ad8254481c6675cafa331462cf32ec8
OlegDemura <demonshine1983@gmail.com>
2019-08-26 22:54:24 +0900

    Fixed CrudVoteRepository.

--- src/main/java/ru/votingsystem/repository/datajpa/CrudVoteRepository.java
+++ src/main/java/ru/votingsystem/repository/datajpa/CrudVoteRepository.java
@@ -1,12 +1,14 @@
 package ru.votingsystem.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.votingsystem.model.Vote;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
+@Repository
 @Transactional(readOnly = true)
 public interface CrudVoteRepository extends JpaRepository<Vote, Integer> {
 
