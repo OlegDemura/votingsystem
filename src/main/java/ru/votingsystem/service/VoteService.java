fc97b54a04301f3c4e86876027862487f6fa8d7a
OlegDemura <demonshine1983@gmail.com>
2019-08-26 23:53:05 +0900

    Fixed VoteService.

--- src/main/java/ru/votingsystem/service/VoteService.java
+++ src/main/java/ru/votingsystem/service/VoteService.java
@@ -3,9 +3,9 @@ package ru.votingsystem.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.votingsystem.model.Vote;
+import ru.votingsystem.repository.datajpa.CrudVoteRepository;
 import ru.votingsystem.repository.datajpa.DataJpaRestaurantRepository;
 import ru.votingsystem.repository.datajpa.DataJpaUserRepository;
-import ru.votingsystem.repository.datajpa.DataJpaVoteRepository;
 import ru.votingsystem.util.exception.VoteRepeatException;
 
 import java.time.LocalDate;
@@ -16,12 +16,12 @@ import static ru.votingsystem.util.DateTimeUtil.DEFAULT_EXPIRED_TIME;
 
 @Service
 public class VoteService {
-    private final DataJpaVoteRepository voteRepository;
+    private final CrudVoteRepository voteRepository;
     private final DataJpaUserRepository userRepository;
     private final DataJpaRestaurantRepository restaurantRepository;
 
     @Autowired
-    public VoteService(DataJpaVoteRepository voteRepository,
+    public VoteService(CrudVoteRepository voteRepository,
                        DataJpaRestaurantRepository restaurantRepository,
                        DataJpaUserRepository userRepository) {
         this.voteRepository = voteRepository;
@@ -29,14 +29,26 @@ public class VoteService {
         this.userRepository = userRepository;
     }
 
-    public Vote vote(Integer userId, Integer restaurantId){
+    public Vote vote(Integer userId, Integer restaurantId) {
         Optional<Vote> votes = voteRepository.findByUserIdAndDateVoting(userId, LocalDate.now());
-        return voteRepository.save(votes.map(v->{
-            if (LocalTime.now().isBefore(DEFAULT_EXPIRED_TIME)){
+        return voteRepository.save(votes.map(v -> {
+            if (LocalTime.now().isBefore(DEFAULT_EXPIRED_TIME)) {
                 throw new VoteRepeatException("today the voting time has expired");
             }
             v.setRestaurant(restaurantRepository.get(restaurantId));
             return v;
         }).orElse(new Vote(LocalDate.now(), userRepository.get(userId), restaurantRepository.get(restaurantId))));
+    }
+
+    public Integer deleteVoteByUserIdAndDateVoting(Integer userId, LocalDate date) {
+        return voteRepository.deleteVoteByUserIdAndDateVoting(userId, date);
+    }
+
+    public Integer countAllByRestaurantIdAndDateVoting(Integer restaurantId, LocalDate date) {
+        return voteRepository.countAllByRestaurantIdAndDateVoting(restaurantId, date);
+    }
+
+    public Integer countAllByRestaurantIdAndDateVotingBetween(Integer restaurantId, LocalDate startDate, LocalDate endDate) {
+        return voteRepository.countAllByRestaurantIdAndDateVotingBetween(restaurantId, startDate, endDate);
     }
 }
