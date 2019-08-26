33801d7ba69723c3b2dd528dce467b3ae4ff435e
OlegDemura <demonshine1983@gmail.com>
2019-08-26 22:04:06 +0900

    Create VoteService.

--- src/main/java/ru/votingsystem/service/VoteService.java
+++ src/main/java/ru/votingsystem/service/VoteService.java
@@ -2,48 +2,41 @@ package ru.votingsystem.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 import ru.votingsystem.model.Vote;
+import ru.votingsystem.repository.datajpa.DataJpaRestaurantRepository;
+import ru.votingsystem.repository.datajpa.DataJpaUserRepository;
 import ru.votingsystem.repository.datajpa.DataJpaVoteRepository;
-import ru.votingsystem.util.exception.NotFoundException;
+import ru.votingsystem.util.exception.VoteRepeatException;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalTime;
+import java.util.Optional;
 
-import static ru.votingsystem.util.ValidationUtil.checkNotFoundWithId;
+import static ru.votingsystem.util.DateTimeUtil.DEFAULT_EXPIRED_TIME;
 
 @Service
 public class VoteService {
-    private final DataJpaVoteRepository repository;
+    private final DataJpaVoteRepository voteRepository;
+    private final DataJpaUserRepository userRepository;
+    private final DataJpaRestaurantRepository restaurantRepository;
 
     @Autowired
-    public VoteService(DataJpaVoteRepository repository) {
-        this.repository = repository;
+    public VoteService(DataJpaVoteRepository voteRepository,
+                       DataJpaRestaurantRepository restaurantRepository,
+                       DataJpaUserRepository userRepository) {
+        this.voteRepository = voteRepository;
+        this.restaurantRepository = restaurantRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<Vote> getAll(int restaurantId) {
-        return repository.getAll(restaurantId);
-    }
-
-    public Vote get(int id, int restaurantId, LocalDate localDate) throws NotFoundException {
-        return checkNotFoundWithId(repository.get(id, restaurantId, localDate), id);
-    }
-
-    public void delete(int id, int userId, int restaurantId) {
-        checkNotFoundWithId(repository.delete(id, userId, restaurantId), id);
-    }
-
-    public void update(Vote vote, int userId, int restaurantId) {
-        Assert.notNull(vote, "vote must not be null");
-        checkNotFoundWithId(repository.save(vote, userId, restaurantId), vote.getId());
-    }
-
-    public Vote create(Vote vote, int userId, int restaurant) {
-        Assert.notNull(vote, "vote must not be null");
-        return repository.save(vote, userId, restaurant);
-    }
-
-    public Integer count(int restaurantId) {
-        return repository.countAllByDateVoting(restaurantId);
+    public Vote vote(Integer userId, Integer restaurantId){
+        Optional<Vote> votes = voteRepository.findByUserIdAndDateVoting(userId, LocalDate.now());
+        return voteRepository.save(votes.map(v->{
+            if (LocalTime.now().isBefore(DEFAULT_EXPIRED_TIME)){
+                throw new VoteRepeatException("today the voting time has expired");
+            }
+            v.setRestaurant(restaurantRepository.get(restaurantId));
+            return v;
+        }).orElse(new Vote(LocalDate.now(), userRepository.get(userId), restaurantRepository.get(restaurantId))));
     }
 }
