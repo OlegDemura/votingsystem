cbfa9c469e9ca8c32d616123b318c76d39551aa0
OlegDemura <demonshine1983@gmail.com>
2019-08-28 23:55:13 +0900

    Fixed MealRestController

--- src/main/java/ru/votingsystem/web/meal/MealRestController.java
+++ src/main/java/ru/votingsystem/web/meal/MealRestController.java
@@ -1,53 +1,68 @@
 package ru.votingsystem.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.votingsystem.model.Meal;
+import ru.votingsystem.service.MealService;
 
 import java.net.URI;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static ru.votingsystem.util.ValidationUtil.assureIdConsistent;
 import static ru.votingsystem.util.ValidationUtil.checkNew;
 
 @RestController
 @RequestMapping(value = MealRestController.REST_URL, produces = APPLICATION_JSON_VALUE)
-public class MealRestController extends AbstractMealController {
+public class MealRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private MealService service;
+
+    @Autowired
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
 
     static final String REST_URL = "/rest/restaurant/meals";
 
-    @Override
     @GetMapping("/{restaurantId}")
     public List<Meal> getAll(@PathVariable int restaurantId) {
-        return super.getAll(restaurantId);
+        log.info("getAll from {}", restaurantId);
+        return service.getAll(restaurantId);
     }
 
-    @Override
     @GetMapping("/{restaurantId}/{id}")
     public Meal get(@PathVariable int id, @PathVariable int restaurantId) {
-        return super.get(id, restaurantId);
+        log.info("get {} from {}", id, restaurantId);
+        return service.get(id, restaurantId);
     }
 
-    @Override
     @DeleteMapping("/{restaurantId}/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id, @PathVariable int restaurantId) {
-        super.delete(id, restaurantId);
+        log.info("delete {} from {}", id, restaurantId);
+        service.delete(id, restaurantId);
     }
 
-    @Override
     @PutMapping(value = "/{restaurantId}/{id}", consumes = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Meal meal, @PathVariable int restaurantId, @PathVariable int id) {
-        super.update(meal, restaurantId, id);
+        log.info("create {} for {}", meal, restaurantId);
+        assureIdConsistent(meal, id);
+        service.update(meal, restaurantId);
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, value = "/{restaurantId}")
     public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal, @PathVariable int restaurantId) {
+        log.info("create {} from {}", meal, restaurantId);
         checkNew(meal);
-        Meal created = super.create(meal, restaurantId);
+        Meal created = service.create(meal, restaurantId);
 
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{restaurantId}/{id}")
