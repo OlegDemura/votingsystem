a80eb447b58249a8e546f70edcee1613a82e85b3
OlegDemura <demonshine1983@gmail.com>
2019-08-29 21:17:19 +0900

    Fixed VoteRestController

--- src/main/java/ru/votingsystem/web/vote/VoteRestController.java
+++ src/main/java/ru/votingsystem/web/vote/VoteRestController.java
@@ -1,11 +1,15 @@
 package ru.votingsystem.web.vote;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.votingsystem.model.Vote;
+import ru.votingsystem.service.VoteService;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -13,19 +17,52 @@ import java.time.LocalTime;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static ru.votingsystem.util.DateTimeUtil.DEFAULT_EXPIRED_TIME;
+import static ru.votingsystem.util.DateTimeUtil.*;
 import static ru.votingsystem.util.ValidationUtil.checkNew;
 import static ru.votingsystem.web.SecurityUtil.authUserId;
 
 @RestController
-@RequestMapping
-public class VoteRestController extends AbstractVoteController {
+@RequestMapping(VoteRestController.REST_URL)
+public class VoteRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     static final String REST_URL = "/rest/vote";
+
+    private VoteService service;
+
+    @Autowired
+    public VoteRestController(VoteService service) {
+        this.service = service;
+    }
 
     @GetMapping(value = "/{restaurantId}")
     @ResponseStatus(value = HttpStatus.OK)
-    public void voting(@PathVariable int restaurantId){
-        super.vote(authUserId(), restaurantId);
+    public void voting(@PathVariable int restaurantId) {
+        log.info("voting for restaurantId {} from userId {}", restaurantId, authUserId());
+        service.vote(authUserId(), restaurantId);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteVoteByUserIdAndDateVoting(){
+        log.info("voting deleted for current date {}", currentDate());
+        service.deleteVoteByUserIdAndDateVoting(authUserId(), currentDate());
+    }
+
+    @GetMapping(value = "/{restaurantId}", consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Integer countAllByRestaurantIdAndDateVoting(@PathVariable int restaurantId, @RequestParam LocalDate localDate){
+        //TODO Need think
+        log.info("count all");
+        return service.countAllByRestaurantIdAndDateVoting(restaurantId, localDate);
+    }
+
+    @GetMapping(value = "/filter/{restaurantId}", consumes = APPLICATION_JSON_VALUE)
+    public Integer countAllByRestaurantIdAndDateVotingBetween(@PathVariable int restaurantId,
+                                                           @RequestParam(required = false) LocalDate startDate,
+                                                           @RequestParam(required = false) LocalDate endDate){
+        //TODO Need think
+        return service.countAllByRestaurantIdAndDateVotingBetween(restaurantId, startDate, endDate);
     }
 
     /*@Override
