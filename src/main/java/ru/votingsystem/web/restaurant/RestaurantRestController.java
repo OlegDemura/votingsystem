295c7b767e9bece24b6819a00ae0cc02fc861fef
OlegDemura <demonshine1983@gmail.com>
2019-08-31 00:11:29 +0900

    Fixed RestaurantRestController

--- src/main/java/ru/votingsystem/web/restaurant/RestaurantRestController.java
+++ src/main/java/ru/votingsystem/web/restaurant/RestaurantRestController.java
@@ -1,56 +1,66 @@
 package ru.votingsystem.web.restaurant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.votingsystem.model.Restaurant;
+import ru.votingsystem.service.RestaurantService;
 
 import java.net.URI;
 import java.util.List;
 
+import static ru.votingsystem.util.ValidationUtil.assureIdConsistent;
 import static ru.votingsystem.util.ValidationUtil.checkNew;
 
 @RestController
 @RequestMapping(value = RestaurantRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
-public class RestaurantRestController extends AbstractRestaurantController {
+public class RestaurantRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     static final String REST_URL = "/rest/profile/restaurants";
 
-    @Override
+    @Autowired
+    protected RestaurantService service;
+
     @GetMapping
     public List<Restaurant> getAll() {
-        return super.getAll();
+        return service.getAll();
     }
 
-    @Override
     @GetMapping("/{id}")
     public Restaurant get(@PathVariable int id) {
-        return super.get(id);
+        log.info("get {}", id);
+        return service.get(id);
     }
 
-    @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
-        super.delete(id);
+        log.info("delete {}", id);
+        service.delete(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        log.info("create {}", restaurant);
         checkNew(restaurant);
-        Restaurant created = super.create(restaurant);
+        Restaurant created = service.create(restaurant);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentRequestUri()
                 .path(REST_URL + "/{id}")
                 .buildAndExpand(created.getId()).toUri();
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
-    @Override
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Restaurant restaurant, @PathVariable int id) {
-        super.update(restaurant, id);
+        log.info("update {} with {}", restaurant, id);
+        assureIdConsistent(restaurant, id);
+        service.update(restaurant);
     }
 }
