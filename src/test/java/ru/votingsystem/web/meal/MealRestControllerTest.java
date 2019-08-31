48b599f580e38b7593b298ff2324267703b6bf72
OlegDemura <demonshine1983@gmail.com>
2019-08-31 22:50:36 +0900

    Fixed MealRestControllerTest

--- src/test/java/ru/votingsystem/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/votingsystem/web/meal/MealRestControllerTest.java
@@ -3,18 +3,22 @@ package ru.votingsystem.web.meal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import ru.votingsystem.model.Meal;
 import ru.votingsystem.service.MealService;
 import ru.votingsystem.web.AbstractControllerTest;
+import ru.votingsystem.web.json.JsonUtil;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.votingsystem.MealTestData.*;
 import static ru.votingsystem.RestaurantTestData.RESTAURANT1_ID;
 import static ru.votingsystem.RestaurantTestData.RESTAURANT2_ID;
+import static ru.votingsystem.TestUtil.readFromJson;
 import static ru.votingsystem.TestUtil.readFromJsonMvcResult;
 
 class MealRestControllerTest extends AbstractControllerTest {
@@ -49,10 +53,28 @@ class MealRestControllerTest extends AbstractControllerTest {
 
     @Test
     void testUpdate() throws Exception {
+        Meal update = new Meal(MEAL1.getId(), MEAL1.getDescription(), MEAL1.getPrice(), MEAL1.getDate());
+        update.setDescription("UpdateDescription");
+        mockMvc.perform(put(REST_URL + RESTAURANT2_ID + "/" + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(update)))
+                .andExpect(status().isNoContent());
 
+        assertMatch(service.get(MEAL1_ID, RESTAURANT2_ID), update);
     }
 
     @Test
     void testCreate() throws Exception {
+        Meal expected = new Meal("New Meal", 750, LocalDate.now());
+        ResultActions action = mockMvc.perform(post(REST_URL + RESTAURANT2_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isCreated());
+
+        Meal returned = readFromJson(action, Meal.class);
+        expected.setId(returned.getId());
+
+        assertMatch(returned, expected);
+        assertMatch(service.getAll(RESTAURANT2_ID), expected, MEAL1, MEAL3, MEAL2);
     }
 }
