25a84baac8876f9a9357dc5a0104707892f8dd2d
OlegDemura <demonshine1983@gmail.com>
2019-09-03 21:42:15 +0900

    Fixed VoteTestData

--- src/test/java/ru/votingsystem/VoteTestData.java
+++ src/test/java/ru/votingsystem/VoteTestData.java
@@ -3,7 +3,6 @@ package ru.votingsystem;
 import ru.votingsystem.model.Vote;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,18 +14,15 @@ import static ru.votingsystem.model.AbstractBaseEntity.START_SEQ;
 
 public class VoteTestData {
     public static final int VOTE1_ID = START_SEQ + 10;
-    public static final int VOTE2_ID = START_SEQ + 11;
-    public static final int VOTE3_ID = START_SEQ + 12;
 
     public static final Vote VOTE1 = new Vote(VOTE1_ID, LocalDate.of(2019, 7, 6), USER, RESTAURANT2);
-    public static final Vote VOTE2 = new Vote(VOTE2_ID, LocalDate.of(2019, 7, 6), ADMIN, RESTAURANT1);
-    public static final Vote VOTE3 = new Vote(VOTE3_ID, LocalDate.of(2019, 7, 8), ADMIN, RESTAURANT1);
+    public static final Vote VOTE2 = new Vote(VOTE1_ID+11, LocalDate.of(2019, 7, 6), ADMIN, RESTAURANT1);
+    public static final Vote VOTE3 = new Vote(VOTE1_ID+12, LocalDate.of(2019, 7, 8), ADMIN, RESTAURANT2);
+    public static final Vote VOTE4 = new Vote(VOTE1_ID+13, LocalDate.of(2019, 7, 9), ADMIN, RESTAURANT1);
+    public static final Vote VOTE5 = new Vote(VOTE1_ID+14, LocalDate.of(2019, 7, 10), ADMIN, RESTAURANT1);
+    public static final Vote VOTE6 = new Vote(VOTE1_ID+15, LocalDate.of(2019, 7, 11), ADMIN, RESTAURANT1);
 
-    public static Vote getUpdate() {
-        Vote vote = new Vote(VOTE1.getId(), VOTE1.getDateVoting(), VOTE1.getUser(), VOTE1.getRestaurant());
-        vote.setRestaurant(RESTAURANT1);
-        return vote;
-    }
+    public static final Integer RESTAURANT1_COUNT = 4;
 
     public static Vote getCreate() {
         return new Vote(null, LocalDate.now(), ADMIN, RESTAURANT1);
