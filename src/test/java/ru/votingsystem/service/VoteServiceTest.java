a5ead459456383ecbf92e3fe0ee5441077d22f3e
OlegDemura <demonshine1983@gmail.com>
2019-09-03 21:42:52 +0900

    Fixed VoteServiceTest

--- src/test/java/ru/votingsystem/service/VoteServiceTest.java
+++ src/test/java/ru/votingsystem/service/VoteServiceTest.java
@@ -6,29 +6,61 @@ import ru.votingsystem.model.Vote;
 import ru.votingsystem.util.exception.VoteRepeatException;
 
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ru.votingsystem.RestaurantTestData.RESTAURANT1_ID;
 import static ru.votingsystem.RestaurantTestData.RESTAURANT2_ID;
+import static ru.votingsystem.UserTestData.ADMIN_ID;
 import static ru.votingsystem.UserTestData.USER_ID;
 import static ru.votingsystem.VoteTestData.*;
+import static ru.votingsystem.TestUtil.*;
 
-public class VoteServiceTest extends AbstractServiceTest {
+class VoteServiceTest extends AbstractServiceTest {
 
     @Autowired
     private VoteService service;
 
     @Test
-    void vote(){
+    void vote() {
         Vote voteCreate = getCreate();
-        Vote newVote = service.vote(USER_ID, RESTAURANT1_ID);
+        Vote newVote = service.vote(USER_ID, RESTAURANT1_ID, LocalTime.now());
         voteCreate.setId(newVote.getId());
         assertMatch(voteCreate, newVote);
     }
 
     @Test
-    void voteRepeat(){
-        service.vote(USER_ID, RESTAURANT1_ID);
-        //TODO Need think how change time for tests
-        assertThrows(VoteRepeatException.class, ()->service.vote(USER_ID, RESTAURANT2_ID));
+    void voteRepeatPossible() {
+        service.vote(USER_ID, RESTAURANT2_ID, LocalTime.of(9, 0));
+        service.vote(USER_ID, RESTAURANT1_ID, LocalTime.of(10, 0));
+    }
+
+    @Test
+    void voteRepeatImpossible() {
+        service.vote(USER_ID, RESTAURANT1_ID, LocalTime.of(9, 0));
+        assertThrows(VoteRepeatException.class, () -> service.vote(USER_ID, RESTAURANT2_ID, LocalTime.of(12, 0)));
+    }
+
+    @Test
+    void deleteVoteByUserIdAndDateVoting() {
+        service.vote(USER_ID, RESTAURANT1_ID, LocalTime.now());
+        service.deleteVoteByUserIdAndDateVoting(USER_ID, LocalDate.now());
+    }
+
+    @Test
+    void countAllByRestaurantIdAndDateVoting() {
+        service.vote(USER_ID, RESTAURANT1_ID, LocalTime.now());
+        service.vote(ADMIN_ID, RESTAURANT1_ID, LocalTime.now());
+        Integer expected = service.countAllByRestaurantIdAndDateVoting(RESTAURANT1_ID, LocalDate.now());
+        assertMatch(2, expected);
+    }
+
+    @Test
+    void countAllByRestaurantIdAndDateVotingBetween() {
+        Integer expected = service.countAllByRestaurantIdAndDateVotingBetween(RESTAURANT1_ID,
+                LocalDate.of(2019,7,6),
+                LocalDate.of(2019,7,12));
+        assertMatch(RESTAURANT1_COUNT, expected);
     }
 }
